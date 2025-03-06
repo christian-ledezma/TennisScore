624c882923b748b7deae3163ac3941cb1259562d
christian-ledezma <chris.ledezma.s@gmail.com>
2025-03-06 09:30:13 -0400

    fear: muestra marcador cuando solo uno es 0

--- src/generador.test.js
+++ src/generador.test.js
@@ -1,7 +1,13 @@
 import generador from "./generadorMarcador.js"
 
-describe("generadorMarcador", () => {
+describe("Marcador puntaje de tennis", () => {
     it("Deberia generar marcador love-love", () => {
         expect(generador(0, 0)).toEqual('love - love');
+    });
+});
+
+describe("Marcador puntaje de tennis", () => {
+    it("Deberia generar marcador 0 con otro marcador", () => {
+        expect(generador(1, 0)).toEqual('1 - 0');
     });
 });
--- src/generadorMarcador.js
+++ src/generadorMarcador.js
@@ -2,6 +2,8 @@ function generador(score_1, score_2){
     let resultado = '';
     if(score_1 == 0 && score_2 == 0)
         resultado = 'love - love';
+    else if (score_1 == 0 || score_2 == 0)
+        resultado = `${score_1} - ${score_2}`;
     return resultado
 }
 
